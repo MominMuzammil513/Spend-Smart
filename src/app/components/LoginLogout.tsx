444be6b52b0dd20f194ff884e6bb2edb9ac1ae10
Muzammil MM <muzammilaumed2376@gmail.com>
2024-11-18 16:18:36 +0530

    update logout btn

--- src/app/components/LoginLogout.tsx
+++ src/app/components/LoginLogout.tsx
@@ -1,10 +1,9 @@
 "use client"
 import { Button } from '@/components/ui/button'
-import { signOut, useSession } from 'next-auth/react'
+import { signOut } from 'next-auth/react'
 import React from 'react'
 
 const LoginLogout = () => {
-    const session = useSession()
     return (
         <div className="group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors">
             {<Button onClick={() => signOut()}>Log Out</Button>}
